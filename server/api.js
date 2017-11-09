124e831f9fc3bfa74e4c63ed16d875e45f10fecb
Charlie <charles.caron@tufts.edu>
2017-11-09 16:10:24 -0500

    adding chuckles api updates

--- server/api.js
+++ server/api.js
@@ -8,6 +8,22 @@ var router = express.Router();
 //database models
 var celebrity = require('./models/celebrity');
 
+router.getTenCelebrities(function(req, res)){
+	celebrity.find({}, function(err, obj){
+		if (err) {
+			return res.status(500);
+		}
+		obj.toArray();
+		var rando = Math.random();
+		for(int i = 0; i < 10; i++){
+			rando = Math.random() * obj.length;
+			json = JSON.stringify(obj[rando]);
+			bigJSON[i] = json;
+		}
+		JSON.stringify(bigJSON);
+	});
+}
+
 router.use(function(req, res, next){
 	console.log("In API");
 	next();
