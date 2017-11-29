b01dcc584adcd2cc223e1b1a9afae60c7a9e3b5b
Aaron Epstein <a.epstein7ae@gmail.com>
2017-11-28 22:05:53 -0500

    Changed error handling

--- app.js
+++ app.js
@@ -41,20 +41,8 @@ app.get('/', function(req,res){
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(404).send("Not Found");
+  next();
 });
 
 app.listen(app.get('port'), function() {
