f3ffc8df3e4cef1989dfcd5a4eee0b5368aa6728
Aaron Epstein <a.epstein7ae@gmail.com>
2017-12-09 08:25:52 -0500

    Added first interaction with edamam api in meal component

--- src/js/meal.js
+++ src/js/meal.js
@@ -11,9 +11,40 @@ export default class Meal extends React.Component {
 	}
 
 
-	$("#cook").click(function(){
+	this.cookClicked = function() {
+		breakfastNutrients = calculateNutritional($('#breakfast').val, $('#breakfast-amount').val);
+		var meals = [
+			new Object({
+				meal: "Breakfast",
+				recipe: $('#breakfast').val
+				amount: $('#breakfast-amount').val
 
-	});
+			}),
+			new Object({
+
+			})
+		];
+	}
+
+	//remove API KEY
+	this.calculateNutritional = function(recipeName, amount) {
+		url = "https://api.edamam.com/search";
+		api_key = "75e18472c4a3e6bfc9fac10d5ce607c7";
+		app_id = "b387bdfd"
+
+		var nutrients = new Object();
+
+		$.get(url, {q: recipeName, app_key: api_key, app_id: app_id, from: 0, to: 1}, function(data){
+			result = JSON.parse(data);
+			food_item = result.hits[0];
+			nutrients.calories = food_item.calories;
+			nutrients.fats = food_item.totalNutrients.FAT.quantity;
+			nutrients.saturated = food_item.totalNutrients.FASAT.quantity;
+			nutrients.carbs = food_item.totalNutrients.CHOCDF.quantity;
+			nutrients.fiber = food_item.totalNutrients.FIBTG.quantity;
+
+		});
+	}
 
 	$("#breakfast").onKeyUp(function(){
 		this.setState({breakfastChosen: true});
@@ -47,8 +78,8 @@ export default class Meal extends React.Component {
 			  }
 			}
 
-			<button onClick="this."
-		)
+			<button onClick="this.cookClicked()"></button>
+		);
 	}
 }
 
