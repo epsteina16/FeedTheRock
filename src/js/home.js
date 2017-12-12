f3418150705b19a130cee24aa511d8c6246a113c
Charlie Caron <charles.caron@tufts.edu>
2017-12-11 23:19:52 -0500

    testing response

--- src/js/home.js
+++ src/js/home.js
@@ -19,8 +19,7 @@ class Main extends React.Component {
 		return (
 			<button onClick={function(){
 				$.get(celeb_url, function(obj, err){
-						JSON.parse(obj);
-						console.log(obj);
+						console.log(obj[0].name);
 						this.state.celebs[0] = obj[0];
 						this.state.celebs[1] = obj[1];
 						this.state.celebs[2] = obj[2];
