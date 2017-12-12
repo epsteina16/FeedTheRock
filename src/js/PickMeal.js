bef2a661a5a7ea8c3f5909cf6a40c66e264634af
Aaron Epstein <a.epstein7ae@gmail.com>
2017-12-12 01:47:46 -0500

    Working pickmeal

--- src/js/PickMeal.js
+++ src/js/PickMeal.js
@@ -2,23 +2,170 @@
 //Used for selecting individual meal
 
 import React from "react";
+import Autosuggest from 'react-autosuggest';
 
-export default class Meal extends React.Component {
+const url = "https://api.edamam.com/search";
+const api_key = "75e18472c4a3e6bfc9fac10d5ce607c7";
+const app_id = "b387bdfd";
+
+// When suggestion is clicked, Autosuggest needs to populate the input
+// based on the clicked suggestion. Teach Autosuggest how to calculate the
+// input value for every given suggestion.
+const getSuggestionValue = suggestion => suggestion.label;
+
+// Use your imagination to render suggestions.
+const renderSuggestion = suggestion => (
+  <div class="suggestion-container">
+  	<div class="suggestion-half">
+  		<img src={suggestion.pic} alt="" />
+  	</div>
+  	<div class="suggestion-half">
+	  	<h5>
+	    {suggestion.label}</h5>
+	    <p>
+	    {suggestion.dietaryRestrictions}</p>
+	</div>
+  </div>
+);
+
+export default class PickMeal extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			mealChosen: false,
+			mealChosen: true,
 			recipes: [],
-			value: ""
+			value: "",
+			image: "",
+			cookingTimeLeft: Math.floor(Math.random() * 20) + 10,
+			nutrients: {}
+		}
+	}
+
+	onChange = (event, { newValue }) => {
+		this.setState({
+			value: newValue,
+			mealChosen: false
+		});
+	};
+
+	// Autosuggest will call this function every time you need to update suggestions.
+	// You already implemented this logic above, so just use it.
+	onSuggestionsFetchRequested = ({ value }) => {
+		if (this.state.value.length == 5 || this.state.value[this.state.value.length - 1] == " ") {
+			$.get(url, {q: this.state.value, app_key: api_key, app_id: app_id, from: 0, to: 100}, function(data){
+				var result = data.hits;
+				var meals;
+				meals = result.map(function(val, index, array){
+					var meal = new Object();
+					meal.label = val.recipe.label;
+					meal.dietaryRestrictions = val.recipe.dietLabels.map(function(value){
+						return " " + value
+					}) + val.recipe.healthLabels.map(function(value){
+						return " " + value
+					});
+					meal.pic = val.recipe.image;
+					meal.nutrition = val.recipe.totalNutrients;
+					meal.nutrition.calories = val.recipe.calories
+					return meal;
+				});
+				this.setState({
+					recipes: meals
+				});
+			}.bind(this));
+		} else {
+			this.setState({
+				recipes: this.state.recipes
+			});
+		}
+	};
+
+	// Autosuggest will call this function every time you need to clear suggestions.
+	onSuggestionsClearRequested = () => {
+		this.setState({
+	  		recipes: []
+		});
+	};
+
+	//called when breakfast is chosen
+	onMealSelected = (event, { suggestion, suggestionValue }) => {
+		this.setState({mealChosen: true, image: suggestion.pic, value: suggestion.label, nutrients: suggestion.nutrition});
+	}
+
+	//called to cook
+	cook() {
+	    $('#pan').removeClass().addClass('shake' + ' animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+	      	$(this).removeClass();
+    	});
+
+		if (this.state.cookingTimeLeft == 0) {
+			var meal = new Object();
+			meal.amount = parseInt($("#meal-amount").val());
+			meal.recipe = this.state.value;
+			meal.nutrients = this.state.nutrients;
+			this.props.chooseMeal(meal);
+		} else {
+			this.setState({cookingTimeLeft: this.state.cookingTimeLeft - 1});
 		}
 	}
 
 	render () {
 		const suggestions = this.state.recipes;
 		const value = this.state.value;
+		const placeholder = this.props.mealName;
+
+		// Autosuggest will pass through all these props to the input.
+	    const inputProps = {
+	      placeholder: placeholder,
+	      value,
+	      onChange: this.onChange
+	    };
 
 		return (
+			<div class="meal">
+				<h2> {this.props.mealName} {this.state.mealChosen ? (": " + this.state.value) : ("")}</h2>
+				<div class="meal-info">
+					<div class="meal-pic">
+						{ this.state.mealChosen ?
+							( <img src={ this.state.image } alt="Meal"></img>) 
+								: (<br />)
+						}
+					</div>
+					<div class="meal-ingredients">
+						<h5>Nutrition Facts per Recipe</h5>
+						<p>Calories: { this.state.nutrients.calories }</p>
+						<p>Fat: { this.state.nutrients.FAT.quantity }g</p>
+						<p>Carbohydrates: { this.state.nutrients.CHOCDF.quantity }g</p>
+						<p>Sugar: {this.state.nutrients.SUGAR.quantity}g</p>
+						<p>Protein: {this.state.nutrients.PROCNT.quantity}g</p>
+					</div>
+				</div>
+				<div class="meal-choose">
+					<Autosuggest
+				        suggestions={suggestions}
+				        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
+				        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
+				        getSuggestionValue={getSuggestionValue}
+				        renderSuggestion={renderSuggestion}
+				        inputProps={inputProps}
+				        onSuggestionSelected={this.onMealSelected}
+				      />
+					{ this.state.mealChosen ? 
+							(<div id="slider"><label for="meal-amount">Number of portions: </label><input id="meal-amount" type="range" min="0.1" max="5" /></div>) 
+							: (<br />)
+					}
+				</div>
 
+				{this.state.mealChosen ? 
+					(
+						<div>
+							<button id="pan-btn" onClick={this.cook.bind(this)}><span id="pan"><img src="images/pan.png" alt="cook"/></span></button>
+							<p>{ this.state.cookingTimeLeft } minutes to cook!</p>
+						</div>
+					) : (
+						<p>Food is fuel!</p>
+					)
+				}
+			</div>
 		)
 	}
 }
--- src/js/meal.js
+++ src/js/meal.js
@@ -1,50 +1,40 @@
 //TODO:
-//autocomplete
-//add search things like gluten free...
-//pictures of food
+//pan animation
+//show nutrition for meal
 
 import React from "react";
-import Autosuggest from 'react-autosuggest';
+import PickMeal from "./PickMeal";
 
 const url = "https://api.edamam.com/search";
 const api_key = "75e18472c4a3e6bfc9fac10d5ce607c7";
 const app_id = "b387bdfd";
-
-// When suggestion is clicked, Autosuggest needs to populate the input
-// based on the clicked suggestion. Teach Autosuggest how to calculate the
-// input value for every given suggestion.
-const getSuggestionValue = suggestion => suggestion.label;
-
-// Use your imagination to render suggestions.
-const renderSuggestion = suggestion => (
-  <div>
-    {suggestion.label}
-    <br />
-    {suggestion.dietaryRestrictions}
-  </div>
-);
 
 
 export default class Meal extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			lunchChosen: true,
-			dinnerChosen: true,
+			lunchChosen: false,
+			lunchEntered: false,
+			dinnerChosen: false,
+			dinnerEntered: false,
 			breakfastChosen: false,
+			breakfastEntered: false,
 			nutrients: {calories: 0, fats: 0, saturated: 0, carbs: 0, fiber: 0, sugar: 0, protein: 0, cholesterol: 0, sodium: 0},
 			breakfast: "",
+			breakfastAmount: 0,
 			lunch: "",
+			lunchAmount: 0,
 			dinner: "",
-			recipes: []
+			dinnerAmount: 0
 		}
 	}
 
 	//called on cook
 	cookClicked() {
-		var breakfast = {recipeName: $('#breakfast').val(), amount: parseInt($('#breakfast-amount').val())};
-		var lunch = {recipeName: $('#lunch').val(), amount: parseInt($('#lunch-amount').val())};
-		var dinner = {recipeName: $('#dinner').val(), amount: parseInt($('#dinner-amount').val())};
+		var breakfast = {recipeName: this.state.breakfast, amount: this.state.breakfastAmount};
+		var lunch = {recipeName: this.state.lunch, amount: this.state.lunchAmount};
+		var dinner = {recipeName: this.state.dinner, amount: this.state.dinnerAmount};
 
 		var fn = this.storeNutrients.bind(this);
 		var process = this.sendData.bind(this);
@@ -69,8 +59,6 @@ export default class Meal extends React.Component {
 	}
 
 	sendData() {
-		//all meals have been processed
-		console.log(this.state.nutrients);
 		this.props.feed(this.state.nutrients);
 	}
 
@@ -81,7 +69,7 @@ export default class Meal extends React.Component {
 		if (result.hits.length == 0){
 			console.log("error");
 		} else {
-			var food_item = result.hits[0].recipe;
+			var food_item = result;
 			nutrients.calories = food_item.calories * quantity;
 			nutrients.fats = food_item.totalNutrients.FAT.quantity * quantity;
 			nutrients.saturated = food_item.totalNutrients.FASAT.quantity * quantity;
@@ -106,183 +94,94 @@ export default class Meal extends React.Component {
 		}
 	}
 
-
-	onChangeBreakfast = (event, { newValue }) => {
-		this.setState({
-			breakfast: newValue
-		});
-	};
-
-	onChangeLunch = (event, { newValue }) => {
-		this.setState({
-			lunch: newValue
-		});
-	};
-
-	onChangeDinner = (event, { newValue }) => {
-		this.setState({
-			dinner: newValue
-		});
-	};
-
-	// Autosuggest will call this function every time you need to update suggestions.
-	// You already implemented this logic above, so just use it.
-	onSuggestionsFetchRequested = ({ value }) => {
-		if (this.state.breakfast.length % 3 == 0) {
-			$.get(url, {q: value, app_key: api_key, app_id: app_id, from: 0, to: 50}, function(data){
-				var result = data.hits;
-				var meals;
-				meals = result.map(function(val, index, array){
-					var meal = new Object();
-					meal.label = val.recipe.label;
-					meal.dietaryRestrictions = val.recipe.dietLabels.map(function(value){
-						return value + " "
-					}) + val.recipe.healthLabels.map(function(value){
-						return value + " "
-					});
-					meal.pic = val.recipe.image;
-					return meal;
-				});
-				this.setState({
-					recipes: meals
-				});
-			}.bind(this));
-		}
-	};
-
-	// Autosuggest will call this function every time you need to clear suggestions.
-	onSuggestionsClearRequested = () => {
-		this.setState({
-	  		recipes: []
-		});
-	};
-
-	//called when breakfast is chosen
-	onBreakfastSelected = (event, { suggestion, suggestionValue }) => {
-		console.log(suggestion);
-		console.log(suggestionValue);
-		this.setState({breakfastChosen: true, breakfastImage: suggestion.pic, breakfast: suggestion.label});
+	//called when breakfast added
+	addBreakfast(breakfast) {
+		storeNutrients(breakfast.nutrients, breakfast.amount).bind(this);
+		this.setState({"breakfastEntered": false, "breakfast": breakfast.recipe, "breakfastAmount": breakfast.amount, "breakfastChosen": true});
 	}
 
-	//called when lunch is chosen
-	onLunchSelected = (event, { suggestion, suggestionValue }) => {
-		this.setState({lunchChosen: true, lunchImage: suggestion.pic, lunch: suggestion.label});
+	makeBreakfast() {
+		this.setState({"breakfastEntered": true});
 	}
 
-	//called when dinner is chosen
-	onBreakfastSelected = (event, { suggestion, suggestionValue }) => {
-		this.setState({dinnerChosen: true, dinnerImage: suggestion.pic, dinner: suggestion.label});
+	//called when lunch added
+	addLunch(lunch) {
+		storeNutrients(lunch.nutrients, lunch.amount).bind(this);
+		this.setState({"lunchEntered": false, "lunch": lunch.recipe, "lunchAmount": lunch.amount, "lunchChosen": true});
 	}
+
+	makeLunch() {
+		this.setState({"lunchEntered": true});
+	}
+
+	//called when dinner added
+	addDinner(dinner) {
+		storeNutrients(dinner.nutrients, dinner.amount).bind(this);
+		this.setState({"dinnerEntered": false, "dinner": dinner.recipe, "dinnerAmount": dinner.amount, "dinnerChosen": true});
+	}
+
+	makeDinner() {
+		this.setState({"dinnerEntered": true});
+	}
+
 
 
 	
 
 	render () {
-		const suggestions = this.state.recipes;
-		const breakfast = this.state.breakfast;
-		const lunch = this.state.lunch;
-		const dinner = this.state.dinner;
 
-		// Autosuggest will pass through all these props to the input.
-	    const inputPropsBreakfast = {
-	      placeholder: 'Breakfast',
-	      value: breakfast,
-	      onChange: this.onChangeBreakfast
-	    };
+		if (this.state.breakfastEntered) {
+			return (
+				<PickMeal mealName = {"Breakfast"} chooseMeal = {this.addBreakfast.bind(this)} />
+			);
+		}
+		else if (this.state.lunchEntered) {
+			return (
+				<PickMeal mealName = {"Lunch"} chooseMeal = {this.addLunch.bind(this)} />
+			);
+		}
+		else if (this.state.dinnerEntered) {
+			return (
+				<PickMeal mealName = {"Dinner"} chooseMeal = {this.addDinner.bind(this)} />
+			);
+		}
+		else {
+			return (
+				<div class="meal-container">
+				<h2>Pick Your Meals</h2>
 
-	    const inputPropsLunch = {
-	      placeholder: 'Lunch',
-	      value: lunch,
-	      onChange: this.onChangeLunch
-	    };
+				{this.state.breakfastChosen ? (
+					<p> { this.state.breakfast } </p>
+				) : (
+					<div>
+					<button onClick={ this.makeBreakfast.bind(this) }>Make Breakfast</button>
+					<br />
+					</div>
+				)}
 
-	    const inputPropsDinner = {
-	      placeholder: 'Dinner',
-	      value: dinner,
-	      onChange: this.onChangeDinner
-	    };
+				{this.state.lunchChosen ? (
+					<p> { this.state.lunch } </p>
+				) : (
+					<div>
+					<button onClick={ this.makeLunch.bind(this) }>Make Lunch</button>
+					<br />
+					</div>
+				)}
+
+				{this.state.dinnerChosen ? (
+					<p> { this.state.dinner } </p>
+				) : (
+					<div>
+					<button onClick={ this.makeDinner.bind(this) }>Make Dinner</button>
+					<br />
+					</div>
+				)}
 
 
-
-		return (
-			<div class="meal-container">
-			<h2>Choose your meals!</h2>
-
-			<div class="meal">
-				<div class="meal-pic">
-					{ this.state.breakfastChosen ?
-						( <img id="breakfastImg" src={ this.state.breakfastImage } alt="Breakfast"></img>) 
-							: (<p>Breakfast is a good way to start the day!</p>)
-					}
+				<button onClick={this.cookClicked.bind(this)} disabled={!this.state.breakfastChosen || !this.state.lunchChosen || !this.state.dinnerChosen}>Cook!</button>
 				</div>
-				<div class="meal-choose">
-					<Autosuggest
-				        suggestions={suggestions}
-				        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-				        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-				        getSuggestionValue={getSuggestionValue}
-				        renderSuggestion={renderSuggestion}
-				        inputProps={inputPropsBreakfast}
-				        onSuggestionSelected={this.onBreakfastSelected}
-				      />
-					{ this.state.breakfastChosen ? 
-							(<div><label for="breakfast-amount">Number of portions: </label><input id="breakfast-amount" type="range" min="1" max="5" /></div>) 
-							: (<p>Not selected</p>)
-					}
-				</div>
-			</div>
-
-			<div class="meal">
-				<div class="meal-pic">
-					{ this.state.lunchChosen ?
-						( <img id="lunchImg" src={ this.state.lunchImage } alt="Lunch"></img>) 
-							: (<p>Lunch is a good way to break up the day!</p>)
-					}
-				</div>
-				<div class="meal-choose">
-					<Autosuggest
-				        suggestions={suggestions}
-				        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-				        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-				        getSuggestionValue={getSuggestionValue}
-				        renderSuggestion={renderSuggestion}
-				        inputProps={inputPropsLunch}
-				        onSuggestionSelected={this.onLunchSelected}
-				      />
-					{ this.state.lunchChosen ? 
-							(<div><label for="lunch-amount">Number of portions: </label><input id="lunch-amount" type="range" min="1" max="5" /></div>) 
-							: (<p>Not selected</p>)
-					}
-				</div>
-			</div>
-
-			<div class="meal">
-				<div class="meal-pic">
-					{ this.state.dinnerChosen ?
-						( <img id="dinnerImg" src={ this.state.dinnerImage } alt="Dinner"></img>) 
-							: (<p>Dinner is a good way to end the day!</p>)
-					}
-				</div>
-				<div class="meal-choose">
-					<Autosuggest
-				        suggestions={suggestions}
-				        onSuggestionsFetchRequested={this.onSuggestionsFetchRequested}
-				        onSuggestionsClearRequested={this.onSuggestionsClearRequested}
-				        getSuggestionValue={getSuggestionValue}
-				        renderSuggestion={renderSuggestion}
-				        inputProps={inputPropsDinner}
-				        onSuggestionSelected={this.onDinnerSelected}
-				      />
-					{ this.state.dinnerChosen ? 
-							(<div><label for="dinner-amount">Number of portions: </label><input id="dinner-amount" type="range" min="1" max="5" /></div>) 
-							: (<p>Not selected</p>)
-					}
-				</div>
-			</div>
-
-			<button onClick={this.cookClicked.bind(this)} disabled={this.state.breakfastChosen && this.state.lunchChosen && this.state.dinnerChosen}>Cook!</button>
-			</div>
-		);
+			);
+		}
 	}
 }
 
