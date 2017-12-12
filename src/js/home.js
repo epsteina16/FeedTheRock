fa5788e711dcbae25326568318b1a15304c4aea2
Charlie Caron <charles.caron@tufts.edu>
2017-12-12 05:54:40 -0500

    checking scores

--- src/js/home.js
+++ src/js/home.js
@@ -10,20 +10,23 @@ class Main extends React.Component {
 	constructor() {
 		super();
 		this.state = {
-			value : -1,
+			value : 0,
 			celebs : undefined,
 			gamestate : 1,
 			celeb_to_pass : undefined,
 			difficulty : undefined,
+			currentDisplay : true,
 		}
 	}
 
 	
 	processScore(score){
 		this.setState({score : this.state.score + score});
+		console.log(score);
 		this.setState({round_check : this.state.round_check + 1});
-		this.setState({value : this.state.value + 1})
-		this.setState({celeb_to_pass : this.state.celebs[this.state.value]})
+		this.setState({value : this.state.value + 1});
+		console.log(this.state.celebs[this.state.value].name);
+		this.setState({celeb_to_pass : this.state.celebs[this.state.value]});
 		if(this.state.round_check == 3){
 			this.state.gamestate = 3;
 		}
@@ -33,12 +36,15 @@ class Main extends React.Component {
 		if(this.state.gamestate == 1){
 			return (
 				<div>
-					<select required id="selected"> 
-						<option value="Easy"> Easy </option>
-						<option value="Medium"> Medium </option>
-						<option value="Hard"> Hard </option> 
+				<h2 class="textcenter"> Feed the Rock </h2>
+				<h3 class="textcenter"> The game of portion control </h3>
+				<h4 class="textcenter_italic">  Can you restrain yourself? </h4>
+					<select class="gobuttoncenter btn-hg textcenter" required id="selected"> 
+						<option value="Easy" class="textcenter"> Easy </option>
+						<option value="Medium" class = "textcenter"> Medium </option>
+						<option value="Hard" class="textcenter"> Hard </option> 
 					</select>
-				<button onClick={function(){
+				<button class="gobuttoncenter btn-info btn-hg" onClick={function(){
 
 					var x = document.getElementById(selected.value);
 					this.setState({difficulty : selected.value});
@@ -51,7 +57,7 @@ class Main extends React.Component {
 							console.log("difficulty is" + this.state.difficulty);
 							this.setState({gamestate : 2});
 					}.bind(this));
-				}.bind(this)}> Start </button>
+				}.bind(this)} > Start </button>
 				</div>
 			);
 		}
