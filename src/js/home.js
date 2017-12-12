d713d9a852fe2d824b31f9bfb34ba7af39f5ef3f
Charlie Caron <charles.caron@tufts.edu>
2017-12-12 02:36:32 -0500

    Push updated changes to home.js -- connecting to other parts of the program

--- src/js/home.js
+++ src/js/home.js
@@ -12,23 +12,67 @@ class Main extends React.Component {
 		this.state = {
 			value : -1,
 			celebs : undefined,
+			gamestate : 1,
+			celeb_to_pass : undefined,
+			difficulty : undefined,
+		}
+	}
+
+	
+	processScore(score){
+		this.setState({score : this.state.score + score});
+		this.setState({round_check : this.state.round_check + 1});
+		this.setState({value : this.state.value + 1})
+		this.setState({celeb_to_pass : this.state.celebs[this.state.value]})
+		if(this.state.round_check == 3){
+			this.state.gamestate = 3;
 		}
 	}
 
 	render() {
-		return (
-			<button onClick={function(){
-				$.get(celeb_url, function(obj, err){
-						console.log(obj[0].name);
-						this.state.celebs[0] = obj[0];
-						this.state.celebs[1] = obj[1];
-						this.state.celebs[2] = obj[2];
-				})
-				console.log("ah");
-				this.setState({value : value + 1});
-				console.log("here!");
-			}}> Start </button>
-		);
+		if(this.state.gamestate == 1){
+			return (
+				<div>
+					<select required id="selected"> 
+						<option value="Easy"> Easy </option>
+						<option value="Medium"> Medium </option>
+						<option value="Hard"> Hard </option> 
+					</select>
+				<button onClick={function(){
+					var x = document.getElementById(selected.value);
+					this.setState({difficulty : selected.value});
+					$.get(celeb_url, function(obj, err){
+							console.log(obj[0].exerciseLevel);
+							console.log(obj[0].partyLevel);
+							this.setState({celebs : obj});
+							this.setState({value : this.state.value + 1});
+							this.setState({celeb_to_pass : this.state.celebs[this.state.value]});
+							console.log("difficulty is" + this.state.difficulty);
+							this.setState({gamestate : 2});
+					}.bind(this));
+				}.bind(this)}> Start </button>
+				</div>
+			);
+		}
+		if(this.state.gamestate == 2){
+			return (
+				<div>
+				<Celebrity setScore = {this.processScore.bind(this)} imageLink = {this.state.celeb_to_pass.imageLink} age = {this.state.celeb_to_pass.age}
+				height = {this.state.celeb_to_pass.height} weight = {this.state.celeb_to_pass.weight}
+				name = {this.state.celeb_to_pass.name} gender = {this.state.celeb_to_pass.gender}
+				isVegan = {this.state.celeb_to_pass.isVegan} isVegetarian = {this.state.celeb_to_pass.isVegetarian}
+				isGlutenFree = {this.state.celeb_to_pass.isGlutenFree} partyLevel = {this.state.celeb_to_pass.partyLevel}
+				exerciseLevel = {this.state.celeb_to_pass.exerciseLevel} difficulty = {this.state.difficulty}/>
+				</div>
+			);
+		}
+		if(this.state.gamestate == 3){
+			return (
+				<div>
+				<Highscore score = {this.state.score}/>
+				</div>
+			)
+		}
 	}
 }
 
